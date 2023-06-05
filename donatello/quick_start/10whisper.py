4fcf852ab28e9d95f8d8fa045a2daf32e8e5081a
Oscar <oscar987123@yahoo.com.tw>
2023-06-05 17:46:16 +0800

    update other lan to speech

--- donatello/quick_start/10whisper.py
+++ donatello/quick_start/10whisper.py
@@ -8,8 +8,11 @@ model = whisper.load_model("base")
 result = model.transcribe("10buildings.mp3")
 print(result["text"])
 
-
 mytext = result["text"]
 audio = gTTS(text=mytext, lang="en", slow=False)
 audio.save("example.mp3")
+
+#tts=gTTS(text='生日快樂', lang='zh')
+#tts=gTTS(text='ありがとう', lang='ja')
+#tts.save("example.mp3")
 os.system("start example.mp3")
