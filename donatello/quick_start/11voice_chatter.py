09c5344f1d927f6d7788d6d1df1424c03c281acb
Oscar <oscar987123@yahoo.com.tw>
2023-06-09 10:52:16 +0800

    update chatter

---
+++ donatello/quick_start/11voice_chatter.py
@@ -0,0 +1,31 @@
+import os
+
+os.environ["OPENAI_API_KEY"] = "sk--QvTPCRd6XraDNcWe7zXHT3BlbkFJ0RzU3K8QaLp4oZ3qwyhe"
+
+from langchain.llms import OpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+
+# openAI
+llm = OpenAI(temperature=0.9, model_name="text-davinci-003")
+conversation = ConversationChain(llm=llm, verbose=True, memory=ConversationBufferMemory())
+
+
+
+while True:
+    user_input = input("請輸入文字：")
+
+    if user_input == "quit":
+        print("程式結束")
+        break
+
+
+    # 在這裡可以根據使用者輸入的文字進行相應的處理
+    #print("你輸入的文字是：", user_input)
+    conversation_output = conversation.predict(input=user_input)
+
+    audio = gTTS(text=conversation_output, lang="en", slow=False)
+    audio.save("response.mp3")
+    os.system("start response.mp3")
+
+
