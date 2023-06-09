6aa591d54ea94c28d43d9cac0c59ff1cb2349f75
Oscar <oscar987123@yahoo.com.tw>
2023-06-09 17:29:05 +0800

    build env file

--- donatello/quick_start/11voice_chatter.py
+++ donatello/quick_start/11voice_chatter.py
@@ -1,17 +1,15 @@
 import os
-
-os.environ["OPENAI_API_KEY"] = "sk--QvTPCRd6XraDNcWe7zXHT3BlbkFJ0RzU3K8QaLp4oZ3qwyhe"
-
+from dotenv import load_dotenv
 from langchain.llms import OpenAI
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 from gtts import gTTS
 
+load_dotenv()
+
 # openAI
 llm = OpenAI(temperature=0.9, model_name="text-davinci-003")
 conversation = ConversationChain(llm=llm, verbose=True, memory=ConversationBufferMemory())
-
-
 
 while True:
     user_input = input("請輸入文字：")
@@ -19,7 +17,6 @@ while True:
     if user_input == "quit":
         print("程式結束")
         break
-
 
     # 在這裡可以根據使用者輸入的文字進行相應的處理
     #print("你輸入的文字是：", user_input)
